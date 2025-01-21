9fe0397a57624974e6444877c2cc782e83de763b
yahya99-lek <yahya.lek99@gmail.com>
2025-01-21 17:25:52 +0100

    clerk authentication setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Button variant="destructive" className="px-10">Click me</Button>
     </main>
   )
 }
